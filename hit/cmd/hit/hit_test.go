d186b266e27f0eabcdfba7b16bf3c3d3cb9d9a25
cyril-ui-developer <cyril.ajieh@gmail.com>
2026-09-15 10:31:54 -0400

    add test setup func

--- hit/cmd/hit/hit_test.go
+++ hit/cmd/hit/hit_test.go
@@ -1,1 +1,19 @@
 package main
+
+import "strings"
+
+type testEnv struct {
+    stdout strings.Builder
+    stderr strings.Builder
+}
+
+func testRun(args ...string) (*testEnv, error) {  
+    var tenv testEnv  
+    err := run(&env{  
+        args:   append([]string{"hit"}, args...), 
+        stdout: &tenv.stdout, 
+      stderr: &tenv.stderr,  
+        dryRun: true,  
+    })
+    return &tenv, err 
+}
