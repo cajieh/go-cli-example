47b4296461d75c2e2950367c1749e6df63b76529
cyril-ui-developer <cyril.ajieh@gmail.com>
2026-09-15 10:58:41 -0400

    add unit tests

--- hit/cmd/hit/hit_test.go
+++ hit/cmd/hit/hit_test.go
@@ -3,11 +3,18 @@ package main
 import (
 "strings"
 "testing"
+"io"
 )
 
 type testEnv struct {
     stdout strings.Builder
     stderr strings.Builder
+}
+
+type parseArgsTest struct {
+    name string
+    args []string
+    want config
 }
 
 func testRun(args ...string) (*testEnv, error) {  
@@ -50,5 +57,47 @@ func TestRunInvalidInput(t *testing.T) {
     }
     if n := tenv.stderr.Len(); n == 0 {  
         t.Error("stderr = 0 bytes; want >0")
+    }
+}
+
+func TestParseArgsValidInput(t *testing.T) {
+    t.Parallel()  
+
+    for _, tt := range []parseArgsTest{
+        {
+            name: "all_flags",
+            args: []string{"-n=10", "-c=5", "-rps=5", "http://test"},
+            want: config{n: 10, c: 5, rps: 5, url: "http://test"},
+        },
+    } {
+t.Run(tt.name, func(t *testing.T) {
+            t.Parallel()  
+
+            var got config
+            if err := parseArgs(&got, tt.args, io.Discard); err != nil {
+                t.Fatalf("parseArgs() error = %v, want no error", err)
+            }
+            if got != tt.want {
+                t.Errorf("flags = %+v, want %+v", got, tt.want)
+            }
+        })
+    }
+}
+
+func TestParseArgsInvalidInput(t *testing.T) {
+    t.Parallel()
+
+    for _, tt := range []parseArgsTest{
+        {name: "n_syntax",   args: []string{"-n=ONE", "http://test"}},
+        {name: "n_zero",     args: []string{"-n=0",   "http://test"}},
+        {name: "n_negative", args: []string{"-n=-1",  "http://test"}},
+    } {
+        t.Run(tt.name, func(t *testing.T) {
+            t.Parallel()
+           err := parseArgs(&config{}, tt.args, io.Discard)
+            if err == nil {
+                t.Fatal("parseArgs() = nil, want error")
+            }
+        })
     }
 }
